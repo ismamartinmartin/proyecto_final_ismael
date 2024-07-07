db7f791ea6842fe1077a6708f53bd3f4a705cb44
ismamartinmartin <ismamartinmartin@alumnos.cei.es>
2024-07-08 01:11:01 +0200

    Carrusel + Autores + Footer
    La home está terminada para ordenador. Se ha terminado las secciones de autores y el footer, y se ha hecho una sección nueva: el carrusel.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
+//MENU VARIABLES
 let modal = document.querySelector(".header__modal")
 let openModal = document.querySelector(".header__content--openModal")
 let cross = document.querySelector(".header__modal__content--close")[0]
+//SLIDER HOME WIDTH > 1000px VARIABLES
+let slider = document.querySelectorAll(".carrousel__slider__container__each")
+let section = document.querySelector(".carrousel__slider__container")
+let contador = 0
 
+//MENU
 openModal.addEventListener('click', () => {
     modal.style.display = "block";
     modal.addEventListener('click', () => {
@@ -11,6 +17,20 @@ openModal.addEventListener('click', () => {
         modal.style.display = "none";  
     })
 })
+
+//SLIDER HOME WIDTH > 1000px
+setInterval(() =>{
+    if (contador < slider.length - 1){
+        contador++
+        let calcule = (contador * (-1000))
+        section.style.transform = (`translateX(${calcule}px)`)
+    }
+    else if (contador == slider.length - 1){
+        contador = 0
+        let calcule = (contador * (-1000))
+        section.style.transform = (`translateX(${calcule}px)`)  
+    }
+},5000)
 
 
 
