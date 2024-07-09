5f7710ba6e5d400b5c17a4ca986226d42752e7a9
ismamartinmartin <ismamartinmartin@alumnos.cei.es>
2024-07-09 17:53:01 +0200

    Search Engine + Stories with L
    Se ha configurado el buscador y finalizado la página Stories with L

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 //MENU VARIABLES
 let modal = document.querySelector(".header__modal")
 let openModal = document.querySelector(".header__content--openModal")
-let cross = document.querySelector(".header__modal__content--close")[0]
+let cross = document.querySelector(".header__modal__content--close")
 //SLIDER HOME WIDTH > 1000px VARIABLES
 let slider = document.querySelectorAll(".carrousel__slider__container__each")
 let section = document.querySelector(".carrousel__slider__container")
@@ -32,5 +32,41 @@ setInterval(() =>{
     }
 },5000)
 
+//SEARCH ENGINE
+function search() {
+    let inputSearch = document.getElementById("inputSearch")
+    let sectionSearch = document.getElementById("sectionSearch")
+    let containerSearch = sectionSearch.querySelectorAll(".cards__container__box")
+    let filter = inputSearch.value.toUpperCase()
+    let matchFound = false
 
+    for (let i = 0; i < containerSearch.length; i++) {
+        let titleSearch = containerSearch[i].getElementsByClassName("cards__container__box__content--information--title")[0]
+        
+        if (titleSearch) {
+            let searchWords = filter.split(' ')
+            let findWords = 0;
 
+            for (let filtering of searchWords) {
+                if (titleSearch.innerHTML.toUpperCase().indexOf(filtering) > -1) {
+                    findWords++
+                }
+            }
+
+            if (findWords === searchWords.length) {
+                containerSearch[i].style.display = ""
+                matchFound = true; 
+            } else {
+                containerSearch[i].style.display = "none"
+            }
+        }
+    }
+
+    let noResultDiv = document.querySelector(".cards__noResult");
+    
+    if (matchFound) {
+        noResultDiv.style.display = "none";
+    } else {
+        noResultDiv.style.display = "flex";
+    }
+}
