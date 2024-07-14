4cd3063b4087287afc3aa518b32ccf68947e1270
ismamartinmartin <ismamartinmartin@alumnos.cei.es>
2024-07-14 19:53:00 +0200

    Media 2
    última adaptación de la página a responsive y últimos cambios

--- js/index.js
+++ js/index.js
@@ -2,16 +2,29 @@
 let modal = document.querySelector(".header__modal")
 let openModal = document.querySelector(".header__content--openModal")
 let cross = document.querySelector(".header__modal__content--close")
-//SLIDER HOME WIDTH > 1000px VARIABLES
+//SLIDER HOME VARIABLES
 let slider = document.querySelectorAll(".carrousel__slider__container__each")
 let section = document.querySelector(".carrousel__slider__container")
 let contador = 0
 
+//MENU
+openModal.addEventListener('click', () => {
+    modal.style.display = "block";
+    modal.addEventListener('click', () => {
+        modal.style.display = "none";
+    })
+    cross.addEventListener('click', () => {
+        modal.style.display = "none";  
+    })
+})
 
-//SLIDER HOME WIDTH > 1000px
+//SLIDER HOME
 function updateSlideWidth() {
-    let slideWidth = document.querySelector('.carrousel__slider__container__each').offsetWidth;
-    section.style.transform = `translateX(${-contador * slideWidth}px)`;
+    let slideElement = document.querySelector('.carrousel__slider__container__each');
+    if (slideElement) {
+        let slideWidth = slideElement.offsetWidth;
+        section.style.transform = `translateX(${-contador * slideWidth}px)`;
+    }
 }
 
 window.addEventListener('resize', updateSlideWidth);
@@ -25,52 +38,39 @@ setInterval(() => {
     updateSlideWidth();
 }, 5000);
 
-//SEARCH ENGINE
-function search() {
-    let inputSearch = document.getElementById("inputSearch")
-    let sectionSearch = document.getElementById("sectionSearch")
-    let containerSearch = sectionSearch.querySelectorAll(".cards__container__box")
-    let filter = inputSearch.value.toUpperCase()
-    let matchFound = false
 
-    for (let i = 0; i < containerSearch.length; i++) {
-        let titleSearch = containerSearch[i].getElementsByClassName("cards__container__box__content--information--title")[0]
+//SEARCH ENGINE
+const search = () => {
+    let inputSearch = document.getElementById("inputSearch");
+    let sectionSearch = document.getElementById("sectionSearch");
+    let containerSearch = sectionSearch.querySelectorAll(".cards__container__box");
+    let filter = inputSearch.value.toUpperCase();
+    let matchFound = false;
+
+    containerSearch.forEach(box => {
+        let titleSearch = box.getElementsByClassName("cards__container__box__content--information--title")[0];
         
         if (titleSearch) {
-            let searchWords = filter.split(' ')
+            let searchWords = filter.split(' ');
             let findWords = 0;
 
-            for (let filtering of searchWords) {
-                if (titleSearch.innerHTML.toUpperCase().indexOf(filtering) > -1) {
-                    findWords++
+            searchWords.forEach(word => {
+                if (titleSearch.innerHTML.toUpperCase().includes(word)) {
+                    findWords++;
                 }
-            }
+            });
 
             if (findWords === searchWords.length) {
-                containerSearch[i].style.display = ""
-                matchFound = true; 
+                box.style.display = "";
+                matchFound = true;
             } else {
-                containerSearch[i].style.display = "none"
+                box.style.display = "none";
             }
         }
-    }
+    });
 
     let noResultDiv = document.querySelector(".cards__noResult");
     
-    if (matchFound) {
-        noResultDiv.style.display = "none";
-    } else {
-        noResultDiv.style.display = "flex";
-    }
-}
+    noResultDiv.style.display = matchFound ? "none" : "flex";
+};
 
-//MENU
-openModal.addEventListener('click', () => {
-    modal.style.display = "block";
-    modal.addEventListener('click', () => {
-        modal.style.display = "none";
-    })
-    cross.addEventListener('click', () => {
-        modal.style.display = "none";  
-    })
-})
